e81d728401b556b89d3b91ef61d88506eb8f59ad
Aleksandr Chugunov <aleksandr.chugunov@evocargo.com>
2025-07-29 20:49:17 +0300

    Update lib

--- utils.go
+++ utils.go
@@ -1,7 +1,16 @@
 package utils
 
-func Contains(line []string, pattern string) bool {
-	for _, n := range line {
+func Contains(lines []string, pattern string) bool {
+	for _, n := range lines {
+		if pattern == n {
+			return true
+		}
+	}
+	return false
+}
+
+func ContainsInt(numbers []int, pattern int) bool {
+	for _, n := range numbers {
 		if pattern == n {
 			return true
 		}
